be1ef16b3ba4b5c8db267512675a0ff31ce6a73c
sujalvelkumar <97998715+sujalvelkumar@users.noreply.github.com>
2022-01-30 13:28:25 +0800

    Add files via upload

---
+++ SolidPattern.java
@@ -0,0 +1,98 @@
+package src.MyPack;
+
+public class SolidPattern{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Start of Block 1
+		for(int r =1; r<=5; r++)
+		{
+			//Print first pattern of *
+			for(int c=5; c>=r; c--)
+			{
+				System.out.print(" ");
+			}
+			
+			for(int c=1; c<=r; c++)
+			{
+				System.out.print("*");
+			}
+			//Print box of spaces
+			for(int c= 1; c<=5; c++)
+			{
+				System.out.print(" ");
+			}
+			//Print second pattern of *
+			for(int c=1; c<=r; c++)
+			{
+				System.out.print("*");
+			}
+			for(int c=5; c>=r; c--)
+			{
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+			//End of Block 1
+			
+			//Start of second block:
+			
+		for(int r=1; r<=5;r++)
+		{
+			//Print box of spaces
+			for(int c =1; c<=6; c++)
+			{
+				System.out.print(" ");
+			}
+			//Print the box of *
+			for(int c= 1; c<=5; c++)
+			{
+				System.out.print("*");
+			}
+			//Print the box of spaces
+			for(int c =1; c<=5; c++)
+			{
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+		//End of Block2
+		
+		//Start of Block3
+		
+		for(int r=1; r<=5; r++)
+		{
+			//Print first pattern of *
+			for(int c=1; c<=r; c++)
+			{
+				System.out.print(" ");
+			}
+			for(int c=5; c>=r; c--)
+			{
+				System.out.print("*");
+			}
+			
+			//Print box of Spaces
+			for(int c= 1; c<=5; c++)
+			{
+				System.out.print(" ");
+			}
+			
+			//Print second pattern of *
+			for(int c=5; c>=r; c--)
+			{
+				System.out.print("*");
+			}
+			
+			for(int c=1; c<=r; c++)
+			{
+				System.out.print(" ");
+			}
+			
+			System.out.println();
+		}
+		
+
+	}
+
+}
